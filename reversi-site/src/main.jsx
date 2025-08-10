5783d4848a1e7581c64102c18a4b8748d0d1c28f
mynameisgua <b12901106@ntu.edu.tw>
2025-08-10 14:13:58 +0800

    second

---
+++ reversi-site/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ReversiApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ReversiApp />
+  </React.StrictMode>
+);
